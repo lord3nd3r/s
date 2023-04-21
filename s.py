9f0d5f448062194b0417dcd59774f6dc8e8280e7
Kristopher Craig <lord3nd3r@gmail.com>
2023-04-20 22:33:22 -0500

    Update s.py

--- s.py
+++ s.py
@@ -58,20 +58,20 @@ if len(sys.argv) == 2 and sys.argv[1] != "":
         result = subprocess.run(["ssh-copy-id", "-i", "~/.ssh/id_rsa.pub", server], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         if result.returncode == 0:
             with open(log_file, "a") as f:
-                f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " - Added SSH key to remote server\n")
-#SSH into server
-    result = subprocess.run(["ssh", "-q", server], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    if result.returncode == 0:
-        with open(log_file, "a") as f:
-            f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + f" - Connected to server {server}\n")
-    else:
-        with open(log_file, "a") as f:
-            f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + f" - Failed to connect to server {server}\n")
-        sys.exit(1)
+                f.write(datetime.datetime.now().strftime("%Y-%m-%d % "%H:%M:%S") + " - Added SSH key to remote server\n")
+# SSH into server
+result = subprocess.run(["ssh", "-q", server], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+if result.returncode == 0:
+with open(log_file, "a") as f:
+f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + f" - Connected to server {server}\n")
 else:
-    with open(log_file, "a") as f:
-        f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + f" - Server not found for nickname {nickname}\n")
-    sys.exit(1)
+with open(log_file, "a") as f:
+f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + f" - Failed to connect to server {server}\n")
+sys.exit(1)
+else:
+with open(log_file, "a") as f:
+f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + f" - Server not found for nickname {nickname}\n")
+sys.exit(1)
 else:
 print("Usage: s nickname")
 with open(log_file, "a") as f:
